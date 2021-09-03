f66d80a29ea1b5ab8207dac9efeedfeaed67d519
Alejm16 <49592023+Alejm16@users.noreply.github.com>
2021-09-02 20:00:51 -0500

    Update Stoppers.py

--- ICE-1/Stoppers.py
+++ ICE-1/Stoppers.py
@@ -4,7 +4,7 @@ nltk.download('stopwords')
 from nltk.corpus import stopwords
 from bs4 import BeautifulSoup
 
-response = urllib.request.urlopen('https://en.wikipedia.org/wiki/SpaceX')
+response = urllib.request.urlopen('https://en.wikipedia.org/wiki/SpaceX')# Looks at the wiki page
 html = response.read()
 soup = BeautifulSoup(html, "html5lib")
 text = soup.get_text(strip=True)
@@ -24,3 +24,4 @@ for key, val in freq.items():
         print(str(key) + ':' + str(val))
 
 freq.plot(10,cumulative =False)
+#freq.tabulate(10) used to get the new visual representation 
